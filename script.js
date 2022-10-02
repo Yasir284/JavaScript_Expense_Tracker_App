296c5b4cdcf0136ab82ada1476880262beb49fd7
Yasir284 <lambawalay@gmail.com>
2022-10-02 20:08:51 +0530

    change css

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 //  Music Player:
 // Build a music player app which has some features like play,
 // pause, Volume-up, volume-down, Previous,next button.
+
+let sufiMusic = [
+  {
+    songNumber: 1,
+    image: "kun_faya_kun.jpeg",
+    songName: "images/kun_faya_kun.jpeg",
+    singer: "A.R. Rahman, Javed Ali, Mohit Chauhan",
+    src: "audios/04. Kun Faya Kun.mp3",
+  },
+  {
+    songNumber: 1,
+    image: "images/Khwaja_mere_khwaja.jpeg",
+    songName: "Khwaja Mere Khwaja",
+    singer: "A.R. Rahman",
+    src: "audios/04. Kun Faya Kun.mp3",
+  },
+  {
+    songNumber: 1,
+    image: "kun_faya_kun.jpeg",
+    songName: "Kun Faya Kun ",
+    singer: "A.R. Rahman, Javed Ali, Mohit Chauhan",
+    src: "audios/04. Kun Faya Kun.mp3",
+  },
+  {
+    songNumber: 1,
+    image: "kun_faya_kun.jpeg",
+    songName: "Kun Faya Kun ",
+    singer: "A.R. Rahman, Javed Ali, Mohit Chauhan",
+    src: "audios/04. Kun Faya Kun.mp3",
+  },
+  {
+    songNumber: 1,
+    image: "kun_faya_kun.jpeg",
+    songName: "Kun Faya Kun ",
+    singer: "A.R. Rahman, Javed Ali, Mohit Chauhan",
+    src: "audios/04. Kun Faya Kun.mp3",
+  },
+];
+
+function play() {
+  document.querySelector(".progress-bar div").classList.add("bar");
+}
